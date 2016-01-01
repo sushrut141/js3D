8de51f3675ad25dad9e81684b78ee643726b5b37
sushrut141 <sushrut.shivaswamy@gmail.com>
2016-01-01 12:00:18 +0530

    Basic Render files

---
+++ Renderer/Shader.js
@@ -0,0 +1,59 @@
+JSENGINE.Shader = function(options)
+{
+	if(!options.vShaderSrc)
+		throw ({'Shader : vShaderSrc is required'});
+	this._vShaderSrc = options.vShaderSrc;
+
+	if(!options.fShaderSrc)
+		throw ({'Shader : fShaderSrc is required'});
+	this._fShaderSrc = options.fShaderSrc;
+
+	if(!options.context)
+		throw ({'Shader : context is required'});
+	this._context = options.context;
+
+	this._gl = this._context._gl;
+
+	this._vShaderID;
+
+	this._fShaderID;
+
+	this._programID;
+
+}
+
+function checkCompileStatus(id)
+{
+	gl.compileShader(id);
+	if(!gl.getShaderParameter(id,gl.COMMPILE_SATUS))
+	{
+		console.log(gl.getShaderInfoLog(id));
+		throw ({'Shader : failed to compile shader'});
+	}
+}
+
+JSENGINE.Shader.prototype.createShaderProgram = function()
+{
+	var gl = this._context._gl;
+	this._programID = gl.createProgram();
+
+	this._vShaderID = gl.createShader(gl.VERTEX_SHADER);
+	this._fShaderID = gl.createShader(gl.FRAGMENT_SHADER);
+
+	gl.shaderSource(this._vShaderID,this._vShaderSrc);
+	gl.shaderSource(this._fShaderId,this._fShaderSrc);
+
+	checkCompileStatus(this._vShaderID);
+	checkCompileStatus(this._fShaderID);
+
+	gl.attachShader(this._programID, this._vShaderID);
+	gl.attachShader(this._programID, this._fShaderID);
+
+	gl.linkProgram(this._programID)
+}
+
+JSENGINE.Shader.prototype.use = function()
+{
+	var gl = this._context._gl;
+	gl.useProggram(this._programID);
+}
